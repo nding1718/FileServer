fa4cbe7868dfa35a35f5f6ef80b89f59d0914d05
nding1718 <nding1718@gmail.com>
2019-01-01 17:27:48 -0800

    build the basic CRUD api for files

--- src/main/java/nding/springboot/FileServer/service/FileService.java
+++ src/main/java/nding/springboot/FileServer/service/FileService.java
@@ -1,4 +1,20 @@
 package nding.springboot.FileServer.service;
 
+import nding.springboot.FileServer.domain.File;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface FileService {
+
+    File saveFile(File file);
+
+
+    void removeFile(String id);
+
+    File getFileById(String id);
+
+
+    List<File> listFilesByPage(int pageIndex, int pageSize);
 }
+
--- src/main/java/nding/springboot/FileServer/service/impl/FileServiceImpl.java
+++ src/main/java/nding/springboot/FileServer/service/impl/FileServiceImpl.java
@@ -1,4 +1,53 @@
 package nding.springboot.FileServer.service.impl;
 
-public class FileServiceImpl {
+import nding.springboot.FileServer.domain.File;
+import nding.springboot.FileServer.domain.FileRepository;
+import nding.springboot.FileServer.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    private FileRepository fileRepository;
+
+    @Autowired
+    public FileServiceImpl(FileRepository fileRepository) {
+        this.fileRepository = fileRepository;
+    }
+
+    @Override
+    public File saveFile(File file) {
+        return fileRepository.save(file);
+    }
+
+    @Override
+    public void removeFile(String id) {
+        fileRepository.deleteById(id);
+    }
+
+    @Override
+    public File getFileById(String id) {
+        return fileRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<File> listFilesByPage(int pageIndex, int pageSize) {
+        Page<File> page = null;
+        List<File> list = null;
+
+        Sort sort = new Sort(Sort.Direction.DESC,"uploadDate");
+        Pageable pageable = new PageRequest(pageIndex, pageSize, sort);
+
+        page = fileRepository.findAll(pageable);
+        list = page.getContent();
+        return list;
+    }
 }
