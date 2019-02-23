fee8b237f45b05b763427321ad6563c4f3960134
nding1718 <nding1718@gmail.com>
2019-02-23 12:43:35 -0800

    fix domain access bug

--- src/main/java/nding/springboot/FileServer/config/SecurityConfig.java
+++ src/main/java/nding/springboot/FileServer/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package nding.springboot.FileServer.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnableWebMvc
+@Configuration
 public class SecurityConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
--- src/main/java/nding/springboot/FileServer/domain/FileRepository.java
+++ src/main/java/nding/springboot/FileServer/domain/FileRepository.java
@@ -4,4 +4,3 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface FileRepository extends MongoRepository<File, String> {
 }
-
