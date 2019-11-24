f35b6400e5593852d82ec3a342cf232871cdec42
nding1718 <nding1718@gmail.com>
2019-11-23 22:49:54 -0800

    add comments

--- src/main/java/nding/springboot/FileServer/utils/MD5Tool.java
+++ src/main/java/nding/springboot/FileServer/utils/MD5Tool.java
@@ -5,6 +5,9 @@ import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Class used to generate the md5 digest for a File object
+ */
 public class MD5Tool {
     public static String getMD5(InputStream stream) throws NoSuchAlgorithmException, IOException {
         StringBuffer md5 = new StringBuffer();
@@ -19,6 +22,7 @@ public class MD5Tool {
 
         // convert the byte to hex format
         for (int i = 0; i < mdbytes.length; i++) {
+            // Get the remainder of origin number % 256 then plus 256
             md5.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
         }
         return md5.toString();
